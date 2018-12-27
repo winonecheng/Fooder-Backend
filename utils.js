3cb1df918a375ed13ba685f67676f924b58db901
winonecheng <F74036328@mail.ncku.edu.tw>
2018-12-27 21:58:30 +0800

    Modify distance formula

--- utils.js
+++ utils.js
@@ -33,17 +33,20 @@ module.exports.connectDB = () => {
 };
 
 module.exports.calDistance = (lat1, lng1, lat2, lng2) => {
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
+
   if (lat1 === lat2 && lng1 === lng2) return 0;
-
-  var radlat1 = Math.PI * lat1/180;
-  var radlat2 = Math.PI * lat2/180;
-  var theta = lng1-lng1;
-  var radtheta = Math.PI * theta/180;
-
-  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  if (dist > 1) dist = 1;
-  dist = Math.acos(dist);
-  dist = dist * 180 / Math.PI;
-  dist = dist * 60 * 1.1515 * 1.609344;
-  return dist;
+  var R = 6371;
+  var dLat = deg2rad(lat2 - lat1);
+  var dLon = deg2rad(lng2 - lng1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
 }
